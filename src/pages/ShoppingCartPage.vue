5a46ee8dd40d1e4f1c125bd7b65fa90edf4846e2
Toni Kong <tkong5012@gmail.com>
2026-07-17 12:44:27 -0500

    Implemented basic shopping cart for website. Added new data for cart items

--- src/pages/ShoppingCartPage.vue
+++ src/pages/ShoppingCartPage.vue
@@ -1,8 +1,27 @@
 <template>
-  <h1>Shopping Cart Page</h1>
+  <h1>Shopping Cart</h1>
+  <div v-if="cartItems.length > 0">
+    <div class="product-container" v-for="item in cartItems" :key="item.id">
+      <img class="product-image" :src="item.imageName"/>
+      <div class="details-wrap">
+        <h3>{{item.name}}</h3>
+        <p>{{item.price}}</p>
+      </div>
+      <button class="remove-button"> Remove from cart</button>
+    </div>
+  </div>
+  <div v-if="cartItems.length === 0"> Please add items to cart!</div>
+  <button class="checkout-button">Proceed to checkout</button>
 </template>
 <script>
+import {cartItems} from "@/temp-data";
+
 export default {
   name: "ShoppingCartPage",
+  data(){
+    return {
+      cartItems,
+    }
+  }
 }
 </script>
